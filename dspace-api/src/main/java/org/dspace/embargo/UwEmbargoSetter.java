54d634cbe1f9695fa2a5cf67abc9d207a67ab4ad
Jeff Sherwood <jeffs3@uw.edu>
2024-05-03 11:30:54 -0700

    Cleanup

--- dspace-api/src/main/java/org/dspace/embargo/UwEmbargoSetter.java
+++ dspace-api/src/main/java/org/dspace/embargo/UwEmbargoSetter.java
@@ -48,6 +48,10 @@ public class UwEmbargoSetter extends DayTableEmbargoSetter
     }
 
 
+    /**
+     * Custom embargo application to accomodate UW_Users permission and whether to apply embargo there
+     */
+    @Override
     protected void generatePolicies(Context context, Date embargoDate,
                                     String reason, DSpaceObject dso, Collection owningCollection)
         throws SQLException, AuthorizeException {
@@ -115,70 +119,6 @@ public class UwEmbargoSetter extends DayTableEmbargoSetter
                     if (rp != null) {
                         // rp.update(); REPLACED WITH BELOW
                         getResourcePolicyService().update(context, rp);
-                    }
-                } else {
-                    log.info("No UW_Users group found, skipping embargo here");
-                }
-            } 
-            else {
-                log.info("Embargo permits UW access, skipping embargo application to UW_Users");
-            }
-
-
-
-        }
-    }
-
-    /**
-     * Custom embargo application to accomodate UW_Users permission and whether to apply embargo there
-     */
-    protected void generatePoliciesx(Context context, Date embargoDate,
-                                    String reason, DSpaceObject dso, Collection owningCollection) throws SQLException, AuthorizeException {
-
-        // add only embargo policy
-        if (embargoDate!=null) {
-
-            Group[] authorizedGroups = AuthorizeManager.getAuthorizedGroups(context, owningCollection, Constants.DEFAULT_ITEM_READ);
-
-            // This should always be true, as UW doesn't apply embargos to collections that are not public-access.  Left here anyways just in case
-            // look for anonymous
-            boolean isAnonymousInPlace=false;
-            for (Group g : authorizedGroups) {
-                if (g.getID()==Group.ANONYMOUS_ID ) {
-                    isAnonymousInPlace=true;
-                    break;
-                }
-            }
-
-            // Embargo anonymous group
-            if (isAnonymousInPlace) {
-                // Anonymous group is always group id "0"
-                ResourcePolicy rp = AuthorizeManager.createOrModifyPolicy(null, context, null, 0, null, embargoDate, Constants.READ, reason, dso);
-                if (rp!=null){
-                    rp.update();
-                }
-            }
-
-            // If embargo also applies to UW_Users, apply embargo there, too
-            if (!lastTerms.contains("Restrict to UW")) {
-
-                // look for UW_Users
-                boolean isUWUsersInPlace=false;
-                Group uwUsers = Group.findByName(context, "UW_Users");
-                int idUWUsers = uwUsers.getID();
-                for(Group g : authorizedGroups) {
-                    if(g.getID()==idUWUsers) {
-                        isUWUsersInPlace=true;
-                        break;
-                    }
-                }
-
-                // Embargo UW_Users
-                if(isUWUsersInPlace) {
-                    log.info("Applying embargo to UW_Users"); 
-                    ResourcePolicy rp = AuthorizeManager.createOrModifyPolicy(null, context, null, idUWUsers, null, embargoDate, Constants.READ, reason, dso);
-                    if (rp!=null) {
-                        rp.update();
                     }
                 } else {
                     log.info("No UW_Users group found, skipping embargo here");
